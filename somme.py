c5444064702c6348a84cd12fd530386fff3dbdd8
jm2meulien <jm2meulien@hotmail.fr>
2018-05-01 14:55:09 +0200

    Create somme.py

---
+++ somme.py
@@ -0,0 +1,11 @@
+# Écrire un programme qui demande à l’utilisateur la saisie de a et b et affiche la somme
+# de a et de b.
+# -*- coding :utf-8 -*-
+def Valeurs(var_a,var_b) :
+print(« Voici le résultat de a + b : »+str(var_c)+ « ! »)
+print(« Donnez-moi la valeur de a »)
+var_a = input ()
+print(« Donnez-moi la valeur de b »)
+var_b = input ()
+var_c=int(var_a)+int(var_b)
+Valeurs(var_a,var_b)
