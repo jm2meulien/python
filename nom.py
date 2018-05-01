8574fc506628fe952d459bb1063b754db26d6e00
jm2meulien <jm2meulien@hotmail.fr>
2018-05-01 15:04:43 +0200

    Update nom.py

--- nom.py
+++ nom.py
@@ -1,7 +1,7 @@
 # Écrire un programme qui permet de saisir le nom de l'utilisateur et de renvoyer
 # "Bonjour", suivi de ce nom.
 
-Nom = raw_input(« Quel est votre nom ? »)
-print « Bonjour » + Nom
+Nom = raw_input("Quel est votre nom ? ")
+print "Bonjour " + Nom
 Ou
-print « Bonjour » + raw_input(« Quel est votre nom ? » )
+print " Bonjour " + raw_input(" Quel est votre nom ? " )
