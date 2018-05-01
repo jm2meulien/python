d38040e322c1b464f9f8c5d9c1d3c0409bf78b43
jm2meulien <jm2meulien@hotmail.fr>
2018-05-01 15:59:26 +0200

    Update factorielle.py

--- factorielle.py
+++ factorielle.py
@@ -1,10 +1,10 @@
 # Fonction factorielle
 # -*- coding:utf-8 -*-
 def fac (n):
-f = 1
-i = 1
-while i <= n:
-f = f * i
-i = i + 1
-return f # la valeur retournée
+  f = 1
+  i = 1
+  while i <= n:
+    f = f * i
+    i = i + 1
+  return f # la valeur retournée
 print(fac(6))
