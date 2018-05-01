b7b97b4bfd10460108d8378fa28f89d6c2fbeb1f
jm2meulien <jm2meulien@hotmail.fr>
2018-05-01 16:06:26 +0200

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -3,7 +3,7 @@
 # -*- coding:utf-8 -*-
 def table(nb, max=10):
 """Fonction affichant la table de multiplication par nb de 1*nb à max*nb (max >=0)"""
-i = 0
-while i < max:
-print(i + 1, "*", nb, "=", (i + 1) * nb)
-i += 1
+  i = 0
+  while i < max:
+    print(i + 1, "*", nb, "=", (i + 1) * nb)
+    i += 1
