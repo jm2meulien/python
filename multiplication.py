f713e584311df9a00f49fa3f9d688c3e0ed1c8cb
jm2meulien <jm2meulien@hotmail.fr>
2018-05-03 23:26:26 +0200

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,9 @@
+# Fonction affichant la table de multiplication
+
+# -*- coding:utf-8 -*-
+def table(nb, max=10):
+  i = 0
+  while i < max:
+    print(i + 1, "*", nb, "=", (i + 1) * nb)
+    i += 1
+table(4,5)
