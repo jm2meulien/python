2840d1a8a95d309300774a345a5c5e86eba05d55
jm2meulien <jm2meulien@hotmail.fr>
2018-05-04 01:09:14 +0200

    Delete somme.py

--- somme.py
+++
@@ -1,9 +0,0 @@
-# -*- coding :utf-8 -*-
-def Valeurs(var_a,var_b) :
-  print(" Voici le résultat de a + b : "+str(var_c)+ " ! ")
-print(" Donnez-moi la valeur de a ")
-var_a = input ()
-print(" Donnez-moi la valeur de b ")
-var_b = input ()
-var_c=int(var_a)+int(var_b)
-Valeurs(var_a,var_b)
