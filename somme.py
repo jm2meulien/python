adf0c2914dc3a02d6796220a80d29651c0fecf75
jm2meulien <jm2meulien@hotmail.fr>
2018-05-04 01:09:52 +0200

    Add files via upload

---
+++ somme.py
@@ -0,0 +1,9 @@
+# -*- coding :utf-8 -*-
+def Valeurs(var_a,var_b):
+	print(" Voici le resultat de a + b : "+str(var_c)+ " ! ")
+print(" Donnez-moi la valeur de a ")
+var_a = input ()
+print(" Donnez-moi la valeur de b ")
+var_b = input ()
+var_c=int(var_a)+int(var_b)
+Valeurs(var_a,var_b)
